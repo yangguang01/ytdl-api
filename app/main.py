4dc9a5d09c9f44e95be5f52b66e4c7c49161ccf7
Yang Guang <yanggaung@YG-MBP.local>
2025-03-20 16:56:40 +0800

    v1.1 添加代理和user-agent

--- app/main.py
+++ app/main.py
@@ -46,8 +46,15 @@ def download_audio_webm(url: str, custom_filename: str) -> str:
         # 强制只下载 webm 格式的最佳音频流
         'format': 'bestaudio[ext=webm]',
         'outtmpl': outtmpl,
-        'quiet': True,
-        'no_warnings': True,
+        #'quiet': True,
+        #'no_warnings': True,
+        'http_headers': {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+        },
+        #'proxy': 'socks5://iGRB7RAM5ZiW46T:dBGZenlkNUslpUg@82.29.116.14:44630',
+        'proxy': 'http://iGRB7RAM5ZiW46T:dBGZenlkNUslpUg@82.29.116.14:44630',
+        # 强制使用IPv4
+        'force_ipv4': True,
     }
     
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
